02def7ed9de69a38355f6fee024f7f1ae0ea22ae
2125813@cegepoutaouais.qc.ca <Princesse003*>
2022-03-11 13:31:50 -0500

    vendredi 11

--- main.py
+++ main.py
@@ -6,13 +6,36 @@ import sys
 import code_interface_genere as F
 
 #Importation des librairies nécessaires à QtDesigne
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 
 class FenetreQt(QtWidgets.QMainWindow,F.Ui_MainWindow):
     def __init__(self, parent = None):
         super(FenetreQt, self).__init__(parent)
         self.setupUi(self)
         self.setWindowTitle("Fenetre principale")
+
+    @QtCore.pyqtSlot()
+    def on_ButtonOk_clicked(self):
+        Texte = self.txtIN.text()
+
+        self.lblOUT.setText("Bonjour " + Texte)
+
+    @QtCore.pyqtSlot()
+    def on_ButtonOk_clicked(self):
+        Texte = self.txtIn.text()
+        self.lblOUT.setText("Bonjour " + Texte)
+
+    def valider_num(self):
+        if self.line_num.text().isalpha():
+
+
+    def valider_nom(self):
+        if self.line_nom.text().isnumeric():
+
+
+
+
+
 def main():
     app = QtWidgets.QApplication(sys.argv)
     form = FenetreQt()
@@ -21,12 +44,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-def on_ButtonOk_clicked(self):
-    Texte = self.txtIN.text()
-
-    self.lblOUT.setText("Bonjour " + Texte)
-
-@QtCore.pyqtSlot()
-def on_ButtonOk_clicked(self):
-    Texte = self.txtIn.text()
-    self.lblOUT.setText("Bonjour " + Texte)
